f58e2e8810f263d61d1290cb5eac83e03926f147
Brent <BrentC0328@gmail.com>
2023-11-16 18:21:38 -0500

    Updated test to confirm conversion working

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,25 @@
 public class RewardValue {
 
+    /** The Reward Value is stored in two instance variables, cashValue and milesValue
+      depending on the constructor used, the converted rate of the other variable
+      is stored in the object upon creation in the corresponding value.
+      **/
     private double cashValue;
     private double milesValue;
 
+    private final double milesToCashConversionRate = .0035;
+
     public RewardValue(double cash){
         this.cashValue = cash;
-        this.milesValue = cashValue / .0035;
+        this.milesValue = cashValue / milesToCashConversionRate;
     }
 
-    public RewardValue(Integer miles){
-        this.cashValue = miles * .0035;
+    public RewardValue(int miles){
+        this.cashValue = miles * milesToCashConversionRate;
         this.milesValue = miles;
     }
+
+
 
     public double getCashValue() {
         return cashValue;
